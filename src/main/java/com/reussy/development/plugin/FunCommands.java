caf3bc414d89ff0983e72a29df97d8cee136f94c
reussy <ricardoaraque@outlook.com>
2022-07-23 13:22:49 -0600

    Initial commit

---
+++ src/main/java/com/reussy/development/plugin/FunCommands.java
@@ -0,0 +1,17 @@
+package com.reussy.development.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FunCommands extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
